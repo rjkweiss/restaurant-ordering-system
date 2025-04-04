7c7539997f48a4a78d881f491ea8213a8fb2ac7d
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-04-04 17:51:32 -0500

    Fix: Replace postgres:// with postgresql:// for Heroku

--- app/config.py
+++ app/config.py
@@ -3,5 +3,10 @@ import os
 
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    uri = os.environ.get("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS=False
